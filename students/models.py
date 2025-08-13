f1fbcfc9c27c4801917c3b846bcb1ac9eb108bf7
Arian Liendro <arian2005lc@gmail.com>
2025-08-13 00:55:25 -0500

    Expansion

--- students/models.py
+++ students/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator 
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-class Student(models.Model):
-    first_name = models.CharField(max_length=30, blank=False)
-    last_name = models.CharField(max_length=30, blank=False)
-    email = models.EmailField(unique=True, blank=False)
+class CustomUser(AbstractUser):
     date_of_birth = models.DateField(blank=True, null=True)
     enrollment_date = models.DateField(auto_now_add=True)
     aura = models.IntegerField(
@@ -12,7 +10,6 @@ class Student(models.Model):
         null=True,
         validators=[MinValueValidator(0), MaxValueValidator(10000)]
     )
-    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
